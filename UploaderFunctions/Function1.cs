c7b93543f956865c053900b39cb2a24e51ec3d11
Saulo Brito <saulo.brito01@outlook.com>
2021-04-16 19:55:10 -0300

    Adicionar arquivos de projeto.

---
+++ UploaderFunctions/Function1.cs
@@ -0,0 +1,27 @@
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace UploaderFunctions
+{
+    public static class Function1
+    {
+        [FunctionName("Function1")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var file = req.Form.Files["file"];
+
+            return new OkObjectResult($"'{file.Name}' has {file.Length}");
+        }
+    }
+}
+
